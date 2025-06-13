e74d500d8cc5707c8badcc11d18cc087abcbd985
Doublado <nikolajjak@pm.me>
2025-06-13 21:59:42 +0200

    Add additional development dependencies to rust.nix; include gcc, pkg-config, openssl, and zlib for enhanced Rust development environment.

--- home/programs/development/rust.nix
+++ home/programs/development/rust.nix
@@ -2,12 +2,17 @@
 
 {
   home.packages = with pkgs; [
+    gcc
+    pkg-config
+    openssl
+    zlib
     rustup
   ];
 
   home.sessionVariables = {
     CARGO_HOME = "${config.home.homeDirectory}/.cargo";
     RUSTUP_HOME = "${config.home.homeDirectory}/.rustup";
+    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.zlib.dev}/lib/pkgconfig";
   };
 
   home.sessionPath = [
